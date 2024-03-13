7c8ea1b6ab1e82eb7e0f5547cca50aed7c57c63c
Lior-Halperin <98975628+Lior-Halperin@users.noreply.github.com>
2024-03-13 11:10:56 +0200

    Add initial NavBar component

---
+++ frontend/src/Components/NavBar/NavBar.styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const StyledWrapper = styled.div`
+  display: flex;
+  color: #ffffff;
+  background-color: #31363f;
+  width: 100%;
+`;
+
+export default StyledWrapper;
---
+++ frontend/src/Components/NavBar/NavBar.tsx
@@ -0,0 +1,9 @@
+import StyledWrapper from "./NavBar.styled";
+
+function NavBar(): JSX.Element {
+  return (
+      <StyledWrapper className="NavBar">NavBar Screen</StyledWrapper>
+  );
+}
+
+export default NavBar; 
