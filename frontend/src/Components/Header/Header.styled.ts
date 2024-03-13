d1761bc5cc1e0be8a83ca2593dcae4e7b32d9ccf
Lior-Halperin <98975628+Lior-Halperin@users.noreply.github.com>
2024-03-13 11:14:39 +0200

    Edited Header component including structure and image animation.

--- frontend/src/Components/Header/Header.styled.ts
+++ frontend/src/Components/Header/Header.styled.ts
@@ -1,11 +1,45 @@
-import styled from 'styled-components';
+import styled, { keyframes } from "styled-components";
 
-interface ComponentsProps {
-    bgColor: string
+interface StyledPropsI {
+  directionSlide: "left" | "right";
+  backgroundColor: "red";
+  animationDelay: number;
 }
 
-const StyledWrapper = styled.div<ComponentsProps>`
-background-color: ${(props) => props.bgColor || '#007bff'};
-`
+export const StyledWrapper = styled.div`
+  display: flex;
+  flex-flow: column wrap;
+  width: 100%;
+`;
 
-export default StyledWrapper;
+export const StyledContainer = styled.div`
+  display: flex;
+  width: 100%;
+  border: 1px solid;
+  border-color: red;
+`;
+
+const getSlidingInImageAnimation = (directionSlide: "left" | "right") => keyframes`
+0% {${directionSlide}:100%}
+100% {${directionSlide}:0%}
+`;
+
+export const StyledImg = styled.img<Partial<StyledPropsI>>`
+  width: 33.333%;
+  position: relative;
+  animation: ${(props) =>
+      getSlidingInImageAnimation(props.directionSlide || "left")}
+       ${(props)=>props.animationDelay}s ease-in-out;
+`;
+
+export const StyledTextContainer = styled.div`
+  display: flex;
+  flex-flow: column wrap;
+  width: 33.33%;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StyledTitle = styled.h1``;
+
+export const StyledParagraph = styled.p``;
--- frontend/src/Components/Header/Header.tsx
+++ frontend/src/Components/Header/Header.tsx
@@ -1,8 +1,32 @@
-import StyledWrapper from "./Header.styled";
+import NavBar from "../NavBar/NavBar";
+import {
+  StyledWrapper,
+  StyledContainer,
+  StyledImg,
+  StyledTextContainer,
+  StyledTitle,
+  StyledParagraph,
+} from "./Header.styled";
+import imgLeft from "../../assets/boxes_with_food.jpg";
+import imgRight from "../../assets/yuval.jpg";
 
 function Header(): JSX.Element {
   return (
-      <StyledWrapper className="Header" bgColor="#28a745">Header Screen</StyledWrapper>
+    <StyledWrapper className="Header">
+      <NavBar />
+      <StyledContainer>
+        <StyledImg src={imgLeft} alt="" directionSlide="right" animationDelay={1}/>
+        <StyledTextContainer>
+          <StyledTitle>סיוע לאוכלוסייה הענייה בישראל</StyledTitle>
+          <StyledParagraph>
+            בואו להדליק יחד איתנו את האור. הנכם מוזמנים לתרום ולהתנדב בחלוקה
+            ואריזת מזון למשפחות נזקקות. (בעמותה אין מקבלי שכר וכל הצוות וההנהלה
+            פועלים בהתנדבות)
+          </StyledParagraph>
+        </StyledTextContainer>
+        <StyledImg src={imgRight} alt="" directionSlide="left" animationDelay={2}/>
+      </StyledContainer>
+    </StyledWrapper>
   );
 }
 
