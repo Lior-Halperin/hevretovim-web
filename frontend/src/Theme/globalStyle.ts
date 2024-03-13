97f6b5d8d03f77c3d4a28541162927f451dcd7e2
Lior-Halperin <98975628+Lior-Halperin@users.noreply.github.com>
2024-03-13 11:00:31 +0200

    Edited globalStyle

--- frontend/src/Theme/globalStyle.ts
+++ frontend/src/Theme/globalStyle.ts
@@ -1,12 +1,12 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    display: flex;
-    font-size: '100px';
+  * {
+    /* background-color: #FF8E8F; */
+    font-size: 1rem;
     margin: 0;
     padding: 0;
   }
-`
+`;
 
 export default GlobalStyle;
